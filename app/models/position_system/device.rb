a1f695f3ee09e68795eff57178a5ce8d9acae017
zhouzaomiao <zzmiao@gmail.com>
2016-03-29 14:42:18 +0800

    add model config

--- app/models/position_system/device.rb
+++ app/models/position_system/device.rb
@@ -1,2 +1,4 @@
 class PositionSystem::Device < ActiveRecord::Base
+  has_many  :logs, :class_name => 'PositionSystem::Log'
+
 end
--- app/models/position_system/log.rb
+++ app/models/position_system/log.rb
@@ -1,2 +1,3 @@
 class PositionSystem::Log < ActiveRecord::Base
+  belongs_to  :device, :class_name => 'PositionSystem::Device'
 end
