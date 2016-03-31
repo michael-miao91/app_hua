b5529fedbe333e7a74f0561dbb1e045f58a3860e
zhouzaomiao <zzmiao@gmail.com>
2016-03-31 09:34:01 +0800

    add order to device logs

--- app/controllers/position_system/devices_controller.rb
+++ app/controllers/position_system/devices_controller.rb
@@ -1,6 +1,6 @@
 class PositionSystem::DevicesController < ApplicationController
   def show
     device = PositionSystem::Device.find(params[:id])
-    @device_log = device.logs
+    @device_log = device.logs.order(created_at: :desc)
   end
 end
