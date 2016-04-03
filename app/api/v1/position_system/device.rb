8650443f841ad15f06be3fa7c208f4f783fd5934
zhouzaomiao <zzmiao@gmail.com>
2016-04-03 11:33:17 +0800

    change save

--- app/api/v1/position_system/device.rb
+++ app/api/v1/position_system/device.rb
@@ -8,8 +8,10 @@ class V1::PositionSystem::Device < Grape::API
       status 200
       obj_arr = params[:data].split(",")
       device = PositionSystem::Device.find_or_create_by(serial_number: obj_arr[0], sim_number: obj_arr[1])
-      lat = obj_arr[3][0..1] + "." + obj_arr[3][2..-1]
-      lng = obj_arr[2][0..2] + "." + obj_arr[2][3..-1]
+      # lat = obj_arr[3][0..1] + "." + obj_arr[3][2..-1]
+      # lng = obj_arr[2][0..2] + "." + obj_arr[2][3..-1]
+      lat = obj_arr[3]
+      lnt = obj_arr[2]
       PositionSystem::Log.create(lng: lng, lat: lat, device_id: device.id)
 
       {success: true}
