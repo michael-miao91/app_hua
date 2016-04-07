ec2edbca62e15b25a248c3f096975990dba17cdb
zhouzaomiao <zzmiao@gmail.com>
2016-04-07 16:00:02 +0800

    开发的时候API修改会自动加载

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
+
 if Rails.env == 'development'
   require 'pp'
 end
@@ -36,6 +28,8 @@ module AppHua
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths.push(*%W(#{config.root}/lib))
+    config.eager_load_paths.push(*%W(#{config.root}/lib))
+
     # Code is not reloaded between requests
     config.cache_classes = false
 
