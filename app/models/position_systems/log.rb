29566d9c2efd379ae8bd5d9d19f98198b5720e69
zhouzaomiao <zzmiao@gmail.com>
2016-04-13 22:19:37 +0800

    add lat lng desc

--- app/models/position_systems/log.rb
+++ app/models/position_systems/log.rb
@@ -1,3 +1,6 @@
 class PositionSystems::Log < ActiveRecord::Base
   belongs_to  :device, :class_name => 'PositionSystems::Device'
+  #lat 维度是小的 -90~90
+  #lng 经度大  -180~180
+  
 end
--- lib/api/position_system/v1/device.rb
+++ lib/api/position_system/v1/device.rb
@@ -13,7 +13,6 @@ module API
             device = PositionSystems::Device.find_or_create_by(serial_number: obj_arr[0], sim_number: obj_arr[1])
             # lat = obj_arr[3][0..1] + "." + obj_arr[3][2..-1]
             # lng = obj_arr[2][0..2] + "." + obj_arr[2][3..-1]
-            #lat是小的
             lng = obj_arr[3]
             lat = obj_arr[2]
             PositionSystems::Log.create(lng: lng, lat: lat, device_id: device.id)
